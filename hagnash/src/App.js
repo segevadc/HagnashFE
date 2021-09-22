436230bd071e0506c512fb34bb1394f336bf228d
segev <segevadc@gmail.com>
2021-09-22 16:42:47 +0300

    add FE

---
+++ hagnash/src/App.js
@@ -0,0 +1,12 @@
+import 'semantic-ui-css/semantic.min.css';
+import {Layout} from "./Layout/Layout";
+
+function App() {
+  return (
+    <div className="App">
+        <Layout />
+    </div>
+  );
+}
+
+export default App;
